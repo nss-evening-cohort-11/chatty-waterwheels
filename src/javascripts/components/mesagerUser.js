76b4fea822cb9a9a29ca1265f3046e7972898c00
Bethany Whitworth <52139891+bwhitworth@users.noreply.github.com>
2020-03-14 17:14:35 -0500

    Delete mesagerUser.js
    duplicate and obsolete file - was part of crossover between tickets adding users

--- src/javascripts/components/mesagerUser.js
+++
@@ -1,8 +0,0 @@
-import message from '../helpers/messageData';
-import newMessage from './newMessage';
-
-const userDisplay = {
-    userId: 'user0.val()'
-    message: $('').val(),
-    messageId: createId.createId(),
-};
