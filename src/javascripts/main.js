0598c0b51aebb11692b2d9e6d36d6a6f8b5bb8ef
Dylan Trevor Rowe <Dylanthk@gmail.com>
2020-03-13 21:13:40 -0500

    dark-theme-final

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,11 +3,13 @@ import '../styles/main.scss';
 import displayMessages from './components/displayMessages';
 import addNewMessage from './components/newMessage';
 import clear from './components/clearMessages';
+import dark from './helpers/data/darkThemeData';
 
 const init = () => {
   displayMessages.displayAllMessages();
   $('body').on('click', '#message-submit-btn', addNewMessage.addNewMessage);
   clear.clearButtonEvent();
+  dark.darkThemeTime();
 };
 
 init();
